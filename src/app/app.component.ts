f3e30f4e683c5035b97b2a0ca95d21c37b59f452
Ashu <ashutosh.softronet@gmail.com>
2019-12-19 14:27:52 +0530

    Done Demo for Security and comment for Encryption in Intercepter..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SafeUrl, SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularSecurity';
+  htmlSnippet = 'Template <script>alert("0wned")</script> <b>Syntax</b>';
+  dangerousUrl: string;
+  trustedUrl: SafeUrl;
+  dangerousVideoUrl: string;
+  videoUrl: SafeResourceUrl;
+  constructor(private sanitizer: DomSanitizer) {
+    this.dangerousUrl = 'javascript:alert("Hi there")';
+    this.trustedUrl = sanitizer.bypassSecurityTrustUrl(this.dangerousUrl);
+    this.updateVideoUrl('PUBnlbjZFAI');
+  }
+  updateVideoUrl(id: string) {
+    // Appending an ID to a YouTube URL is safe.
+    // Always make sure to construct SafeValue objects as
+    // close as possible to the input data so
+    // that it's easier to check if the value is safe.
+    this.dangerousVideoUrl = 'https://www.youtube.com/embed/' + id;
+    this.videoUrl =
+      this.sanitizer.bypassSecurityTrustResourceUrl(this.dangerousVideoUrl);
+  }
 }
+//Note:-
+/**
+ * We can use end to end Encryption -> we send request using encryption 
+ * in Intercepter _> decrypt it in Server
+ * Same in response we decrypt the response in client side.
+ */
